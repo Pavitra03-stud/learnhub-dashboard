537b826d87ba1b1831d54c212b7948ee1359c39a
unknown <Pavitragv24cs@srishakthi.ac.in>
2026-05-30 19:12:08 +0530

    Redirect home page to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import { supabase } from "../src/lib/supabase";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const { data, error } = await supabase
-    .from("courses")
-    .select("*");
-
-  return (
-    <main style={{ padding: "20px" }}>
-      <h1>Supabase Test</h1>
-
-      {error && (
-        <pre>{JSON.stringify(error, null, 2)}</pre>
-      )}
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </main>
-  );
+export default function Home() {
+  redirect("/dashboard");
 }
